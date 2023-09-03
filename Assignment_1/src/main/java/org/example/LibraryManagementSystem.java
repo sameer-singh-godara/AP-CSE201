1cf16672327b9644bed7b8193be5cfdd1578e4ab
Sameer Singh Godara <sameer22439@iiitd.ac.in>
2023-09-03 17:55:28 +0530

    main renamed to library management system

--- Assignment_1/src/main/java/org/example/LibraryManagementSystem.java
+++ Assignment_1/src/main/java/org/example/LibraryManagementSystem.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class LibraryManagementSystem {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Library library = new Library();
